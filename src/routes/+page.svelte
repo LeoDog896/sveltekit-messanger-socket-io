2f357f638cd3b74c11b8357119527e7009e523f8
Tristan F. <LeoDog896@hotmail.com>
2023-10-24 16:58:18 +0000

    feat: base chat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { io } from "socket.io-client";
+
+    interface Message {
+        author: string;
+        content: string;
+    }
+
+    let message = '';
+
+    const messages: Message[] = []
+    const socket = io("sockets.hosted.costerfan5.com");
+
+    function submit() {
+        messages.push({
+            author: "me",
+            content: message
+        })
+        socket.emit("message", message);
+    }
+
+    socket.on("message", message => {
+        // TODO
+    })
+</script>
+
+<div>
+    {#each messages as message}
+        <p>{message.author}: {message.content}</p>
+    {/each}
+</div>
+
+<input bind:value={message} />
+<button on:click={submit}>Submit</button>
